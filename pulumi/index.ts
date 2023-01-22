46c4301f0b1b644dcdd60359da3cf7c79e58f3d5
Lingyong Sun <lingyong.sun@gmail.com>
2023-01-22 21:10:52 +0100

    add destination dependencies

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -2,8 +2,16 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
+const configs = new pulumi.Config();
+const stack = pulumi.getStack();
+const org = pulumi.getOrganization();
+const ec2Ref = new pulumi.StackReference(`${org}/${configs.require('dest')}/${stack}`)
+
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+// const bucket = new aws.s3.Bucket("my-bucket");
 
 // Export the name of the bucket
-export const bucketName = bucket.id;
+export const connection = {
+    user: ec2Ref.getOutput('user'),
+    host: ec2Ref.getOutput('publicIp')
+}
